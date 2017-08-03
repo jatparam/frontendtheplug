7d1c5c823f7ce9be0e2556d6601cb4c1b936ffe6
jatparam <jatharsan.param@gmail.com>
2017-08-03 14:17:00 -0400

    first commit

---
+++ Components/FrontPage/frontpage.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+
+class LogoImg extends Component {
+  render() {
+    return (
+      <Image source={require('./assets/img/34481-200.png')} />
+    );
+  }
+}
---
+++ index.android.js
@@ -0,0 +1,68 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+import {
+  Button,
+  PricingCard,
+  SearchBar,
+  SideMenu,
+  List,
+  ListItem
+} from 'react-native-elements';
+
+class LogoImg extends Component {
+  render() {
+    return (
+      <Image source={require('./assets/img/34481-200.png')} />
+    );
+  }
+}
+
+
+
+export default class TestProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <LogoImg />
+        <Text style={styles.instructions}>
+          Create. Connect
+        </Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+  welcome: {
+    fontSize: 20,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FFFFFF',
+  },
+  instructions: {
+    textAlign: 'center',
+    color: '#FFFFFF',
+    marginBottom: 5,
+  },
+});
+
+AppRegistry.registerComponent('TestProject', () => TestProject);
