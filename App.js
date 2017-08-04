1476036f9112553bf587645ce727db61474d6a16
jatparam <jatharsan.param@gmail.com>
2017-08-04 14:36:21 -0400

    moved everything into app.js

---
+++ App.js
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View,
+  Image
+} from 'react-native';
+
+
+
+import FrontPage from './Components/FrontPage/FrontPage.js'
+import ProjectPage from './Components/ProjectPage/ProjectPage.js'
+import ProfilePage from './Components/UserProfile/ProfilePage.js'
+
+export default class TestProject extends Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <ProjectPage />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000000',
+  },
+});
+
+AppRegistry.registerComponent('TestProject', () => TestProject);
