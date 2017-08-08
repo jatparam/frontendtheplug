13e3baa034d60aa841b4d7a3bf7417285c8a673d
jatparam <jatharsan.param@gmail.com>
2017-08-08 02:51:19 -0400

    deleted unnecessary components

--- config/router.js
+++ config/router.js
@@ -1,17 +1,10 @@
 import React from 'react';
 import { StackNavigator } from 'react-navigation';
 
-// These are the four we need
 import FrontPage from '../Components/FrontPage/FrontPage.js';
 import Feed from '../Components/DiscoverFeed/Feed.js';
 import MyProjectPage from '../Components/MyProjectPage/MyProjectPage.js';
 import ProfilePagex from '../Components/ProfilePage/ProfilePage.js';
-
-// Should be able to delete this
-import ProfilePage from '../Components/UserProfile/ProfilePage.js';
-import ProjectPage from '../Components/ProjectPage/ProjectPage.js';
-import Projects from '../Components/UserProfile/Projects.js';
-
 
 export const MainStack = StackNavigator({
   FrontPage: {
